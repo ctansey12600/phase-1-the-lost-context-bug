e9d37c6ceee490823cb30e598aea7ab01dba7866
caseytansey <ctansey12600@outlook.com>
2021-09-09 22:29:40 -0500

    Completed Assignment

--- index.js
+++ index.js
@@ -10,3 +10,74 @@ const messageConfig = {
       "Loki"
   ]
 };
+// Code that does not work:
+// const printCard = function () {
+//   console.log(this.frontContent);
+//   console.log(this.insideContent);
+
+//   this.signatories.forEach(function (signatory) {
+//     const message = `${this.closing[signatory]}, ${signatory}`;
+//     console.log(message);
+//   });
+// };
+
+//printCard.call(messageConfig);
+
+//Uses this as a second argument:
+// const printCard = function () {
+//   console.log(this.frontContent)
+//   console.log(this.insideContent)
+
+//   this.signatories.forEach(function(signatory) {
+//     const message = `${this.closing[signatory]}, ${signatory}`
+//     console.log(message)
+//   }, this)
+//   //This function now works because this is added as a agrument for the forEach 
+//   //which now understands what this.signatories is
+// }
+
+// printCard.call(messageConfig)
+
+//with bind
+
+// const printCard = function () {
+//   console.log(this.frontContent)
+//   console.log(this.insideContent)
+//   const contextBoundForEachExpr = function (signatory) {
+//     const message = `${this.closing[signatory]}, ${signatory}`
+//     console.log(message)
+//   }.bind(this)
+
+//   this.signatories.forEach(contextBoundForEachExpr)
+// }
+
+// printCard(messageConfig)
+
+//Using a closure to regain access to the lost context:
+
+// const printCard = function () {
+//   console.log(this.frontContent)
+//   console.log(this.insideContent)
+
+//   const outerContext = this
+
+//   this.signatories.forEach(function (signatory) {
+//     const message = `${outerContext.closing[signatory]}, ${signatory}`
+//     console.log(message)
+//   })
+// }
+
+// printCard.call(messageConfig)
+
+//Using an Arrow Function
+//can not use call,bind,apply on arrow function
+
+const printCard = function() {
+  console.log(this.frontContent)
+  console.log(this.insideContent)
+  this.signatories.forEach((signatory) => 
+  console.log(`${this.closing[signatory]}, ${signatory}`)
+  )
+}
+
+printCard.call(messageConfig)
